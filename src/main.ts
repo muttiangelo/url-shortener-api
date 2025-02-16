856149e29f1fdea2b99ac14805a76b4236754242
angelomutti <amutti.dev@gmail.com>
2025-02-15 22:55:21 -0300

    first commit
    created project with nest cli

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Url Shortener API')
+    .setDescription('The Url Shortener  API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
---
+++ src/url/dto/create-url.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUrlDto {
+  @ApiProperty({ required: true, description: 'The original URL' })
+  originalUrl: string;
+
+  @ApiProperty({
+    required: false,
+    description:
+      'The custom code for the shortened URL. You can choose one or let us create randomly.',
+  })
+  customCode?: string;
+
+  @ApiProperty({ required: false, description: 'A nickname for this url' })
+  alias?: string;
+
+  @ApiProperty({
+    required: false,
+    description: 'The expiration date of the shortened URL',
+  })
+  expiresAt?: Date;
+}
